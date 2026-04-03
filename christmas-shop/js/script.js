68b0db32fd4a0ab385a041e21b14c2b928f3a08a
Hanna <hannamuzychuk@gmail.com>
2026-04-03 14:27:45 +0200

    feat: add script and update gifts page

---
+++ christmas-shop/js/script.js
@@ -0,0 +1,26 @@
+const targetDate = new Date("Jan 1, 2027 00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = targetDate - now;
+
+  document.getElementById("days").textContent =
+    Math.floor(diff / (1000 * 60 * 60 * 24));
+  document.getElementById("hours").textContent =
+    Math.floor((diff / (1000 * 60 * 60)) % 24);
+  document.getElementById("minutes").textContent =
+    Math.floor((diff / (1000 * 60)) % 60);
+  document.getElementById("seconds").textContent =
+    Math.floor((diff / 1000) % 60);
+}, 1000);
+
+
+// 1. Znajdź wszystkie linki wewnątrz nawigacji tabs
+  const navLinks = document.querySelectorAll('.tabs a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+      navLinks.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
